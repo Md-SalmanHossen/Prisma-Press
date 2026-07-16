c7e326cae7802de21a313ee89e534d4e445a14a5
SalmanHossen <hossainsalman873@gmail.com>
2026-07-16 11:35:22 +0600

    (model) : create user and profile schema model

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { type Express, type Request, type Response } from 'express';
 import config from './config';
+import { prisma } from './lib/prisma';
 
 const app: Express = express();
 
@@ -14,7 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cookieParser());
 
-app.get("/",(req:Request,res:Response)=>{
+app.get("/",async(req:Request,res:Response)=>{
+   const user=await prisma.user.findMany();
+   console.log(user);
    res.send("Hello world");
 });
 
