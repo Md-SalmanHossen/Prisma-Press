aa2cdf8efb579a225682dbbc1953d5fe0e104f71
SalmanHossen <hossainsalman873@gmail.com>
2026-07-16 23:32:45 +0600

    (user) : implement user registration login in moduler pattern

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,7 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express, { type Express, type Request, type Response } from 'express';
 import config from './config';
-import { prisma } from './lib/prisma';
-
+import userRoutes from './modules/user/user.route'
 const app: Express = express();
 
 app.use(cors({
@@ -16,9 +15,11 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 
 app.get("/",async(req:Request,res:Response)=>{
-   const user=await prisma.user.findMany();
-   console.log(user);
    res.send("Hello world");
 });
+
+
+app.use("/api/users",userRoutes)
+
 
 export default app;
---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,32 @@
+import {type Request,type Response} from "express";
+import httpStatus from 'http-status';
+import { userService } from "./user.service";
+
+const createUser =async(req:Request,res:Response)=>{
+   try {
+      const payload=req.body;
+
+      const user =await userService.registerUserIntoDB(payload);
+      
+      res.status(httpStatus.CREATED).json({
+         success:true,
+         statusCode:httpStatus.CREATED,
+         message:"User registered successfully",
+         data:{
+            user
+         }
+      });
+   } catch (error : any) {
+      console.log(error)
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+         success:false,
+         statusCode:httpStatus.INTERNAL_SERVER_ERROR,
+         message:'Failed to register user',
+         error:(error).message
+      })
+   }
+}
+
+export const userController={
+   createUser,
+}
