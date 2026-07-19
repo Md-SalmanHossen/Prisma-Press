5d3ba14a621f6cf5aaa3ee6ab5eaecde7ec643f1
SalmanHossen <hossainsalman873@gmail.com>
2026-07-19 13:11:47 +0600

    (auth) : implement cookies

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,6 +7,22 @@ import  httpStatus  from 'http-status';
 const loginUser=catchAsync(async(req:Request,res:Response,next:NextFunction)=>{
    const payload=req.body;
 
+   const {accessToken,refreshToken}= await authService.loginUser(payload);
+
+   res.cookie("accessToken",accessToken,{
+      httpOnly:true,
+      secure:false,
+      sameSite:"none",
+      maxAge: 1000*60*60*24
+   });
+
+   res.cookie("refreshToken",refreshToken,{
+      httpOnly:true,
+      secure:false,
+      sameSite:"none",
+      maxAge:1000*60*60*24*7
+   });
+
    const loginResult= await authService.loginUser(payload);
 
    sendResponse(res,{
